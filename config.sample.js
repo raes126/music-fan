e0ddddc1ee729d5233ce241ed9fe1fbe8c143a0c
AKA-IT <76665354+AKAIT00@users.noreply.github.com>
2021-03-04 14:15:47 +0300

    Update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -1,17 +1,17 @@
 module.exports = {
 	/* The token of your Discord Bot */
-	token: "XXXXXXXXXXX",
+	token: "NzczMjg3NDk3MzE2NDMzOTYz.X6HCGg.GonGSgC3VVNv0OAa2L3RGoNV9Pk",
 	/* For the support server */
 	support: {
-		id: "XXXXXXXXXXX", // The ID of the support server
-		logs: "XXXXXXXXXXX", // And the ID of the logs channel of your server (new servers for example)
+		id: "759525742747844628", // The ID of the support server
+		logs: "809828624718233611", // And the ID of the logs channel of your server (new servers for example)
 	},
 	/* Dashboard configuration */
 	dashboard: {
 		enabled: false, // whether the dashboard is enabled or not
 		secret: "XXXXXXXXXXX", // Your discord client secret
 		baseURL: "https://dashboard.atlanta-bot.fr", // The base URl of the dashboard
-		logs: "XXXXXXXXXXX", // The channel ID of logs
+		logs: "809828624718233611", // The channel ID of logs
 		port: 8080, // Dashboard port
 		expressSessionPassword: "XXXXXXXXXXX", // Express session password (it can be what you want)
 		failureURL: "https://www.atlanta-bot.fr" // url on which users will be redirected if they click the cancel button (discord authentication)
